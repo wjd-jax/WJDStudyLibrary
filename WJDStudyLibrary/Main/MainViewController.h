49a24bd12dd198cee9ce26f274ba770fbea80284
wangjundong <wangjundong@hanyihan.com>
2017-03-24 17:19:51 +0800

    创建目录结构

--- WJDStudyLibrary/Main/MainViewController.h
+++ WJDStudyLibrary/Main/MainViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  MainViewController.h
 //  WJDStudyLibrary
 //
 //  Created by wangjundong on 2017/3/24.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface MainViewController : UIViewController
 
 @end
-
