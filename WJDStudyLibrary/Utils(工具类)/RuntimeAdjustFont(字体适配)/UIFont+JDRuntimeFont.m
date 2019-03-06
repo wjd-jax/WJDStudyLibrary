0c6f66ed4040465f6639134fc332e142e00d308f
wangjundong <wangjundong.vip@qq.com>
2019-03-06 10:13:35 +0800

    提交代码

--- WJDStudyLibrary/Utils(工具类)/RuntimeAdjustFont(字体适配)/UIFont+JDRuntimeFont.m
+++ WJDStudyLibrary/Utils(工具类)/RuntimeAdjustFont(字体适配)/UIFont+JDRuntimeFont.m
@@ -13,20 +13,41 @@
 
 @implementation UIFont (JDRuntimeFont)
 
-+ (void)load{
-    //获取替换后的类方法
-    Method newMethod =class_getClassMethod([self class], @selector(adjustFont:));
-    //获取需要替换的类方法
++ (void)load {
+    
+    //正常
+    Method newMethod = class_getClassMethod([self class], @selector(adjustFont:));
     Method method = class_getClassMethod([self class], @selector(systemFontOfSize:));
-    //交换方法
     method_exchangeImplementations(newMethod, method);
-
+    
+    //粗体
+    Method adjustBlodFont = class_getClassMethod([self class], @selector(adjustBlodFont:));
+    Method boldSystemFontOfSize = class_getClassMethod([self class], @selector(boldSystemFontOfSize:));
+    method_exchangeImplementations(adjustBlodFont, boldSystemFontOfSize);
+    
+    //自定义字体
+    Method adjustNameFont = class_getClassMethod([self class], @selector(adjustNameFont:size:));
+    Method NameFontOfSize = class_getClassMethod([self class], @selector(fontWithName:size:));
+    method_exchangeImplementations(adjustNameFont, NameFontOfSize);
 }
 
-+ (UIFont *)adjustFont:(CGFloat)fontSize{
-    UIFont *newFont=nil;
-    newFont = [UIFont adjustFont:fontSize * [UIScreen mainScreen].bounds.size.width/UISize_With];
++ (UIFont *)adjustFont:(CGFloat)fontSize {
+    UIFont *newFont = nil;
+    newFont = [UIFont adjustFont:fontSize * [UIScreen mainScreen].bounds.size.width / UISize_With];
     return newFont;
 }
+
++ (UIFont *)adjustBlodFont:(CGFloat)fontSize {
+    UIFont *newFont = nil;
+    newFont = [UIFont adjustBlodFont:fontSize * [UIScreen mainScreen].bounds.size.width / UISize_With];
+    return newFont;
+}
+
++ (UIFont *)adjustNameFont:(NSString *)fontName size:(CGFloat)fontSize {
+    UIFont *newFont = nil;
+    newFont = [UIFont adjustNameFont:fontName size:fontSize * [UIScreen mainScreen].bounds.size.width / UISize_With];
+    return newFont;
+}
+
 
 @end
